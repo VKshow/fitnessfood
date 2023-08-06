030c901aaf2cca86b515c78c276f3dbd0ef02559
Vlad <justvk2013@gmail.com>
2023-08-06 15:09:09 -0600

    classes

--- js/script.js
+++ js/script.js
@@ -142,7 +142,7 @@ function getTimeRemaining(endtime){
             }
         });
 
-        const modalTimerId = setTimeout(openModal, 5000);
+        // const modalTimerId = setTimeout(openModal, 5000);
 
         function showModalByScroll(){
             if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1){
@@ -152,4 +152,70 @@ function getTimeRemaining(endtime){
         }
 
         window.addEventListener('scroll', showModalByScroll);
+
+
+        // use classes for cards
+
+        class MenuCard{
+            constructor(src, alt, title, descr, price, parentSelector){
+                this.src = src;
+                this.alt = alt;
+                this.title = title;
+                this.descr = descr;
+                this.price = price;
+                this.parent = document.querySelector(parentSelector);
+                this.transfer = 1;
+                this.changeToUAH();
+            }
+
+            changeToUAH(){
+                this.price = this.price / this.transfer;
+            }
+
+            render(){
+                const element = document.createElement('div');
+                element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">
+                        ${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Price:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> usd/day</div>
+                    </div>
+                </div>
+                `;
+                this.parent.append(element);
+            }
+        }
+
+        new MenuCard(
+            "img/tabs/vegy.jpg" ,
+            "vegy",
+            'Menu "Fitness"',
+            'Menu "Fitness" is a new approach to cooking: more fresh vegetables and fruits. Product of active and healthy people. This is a brand new product with the best price and high quality!',
+            50,
+            '.menu .container'
+        ).render();
+
+        new MenuCard(
+            "img/tabs/elite.jpg" ,
+            "elite",
+            'Menu "Premium"',
+            'In the “Premium” menu, we use not only beautiful packaging design, but also high-quality execution of dishes. Red fish, seafood, fruits - a restaurant menu without going to a restaurant!',
+            70,
+            '.menu .container'
+        ).render();
+
+        new MenuCard(
+            "img/tabs/post.jpg" ,
+            "post",
+            'Menu "Lenten"',
+            'The “Lenten” menu is a careful selection of ingredients: the complete absence of animal products, milk from almonds, oats, coconut or buckwheat, the right amount of protein from tofu and imported vegetarian steaks.',
+            100,
+            '.menu .container'
+        ).render();
+
 });
