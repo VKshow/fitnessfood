437109c2671f0d1ea6e611d20daf4ea64fdbc896
vk <justvk2013@gmail.com>
2023-08-30 20:41:20 -0600

    create a slider

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 //timer
 
-const deadline = '2023-09-09';
+const deadline = '2023-11-11';
 
 function getTimeRemaining(endtime){
     const t = Date.parse(endtime) - Date.parse(new Date()), 
@@ -143,7 +143,7 @@ function getTimeRemaining(endtime){
             }
         });
 
-        const modalTimerId = setTimeout(openModal, 100000);
+        const modalTimerId = setTimeout(openModal, 5000);
 
         function showModalByScroll(){
             if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1){
@@ -157,51 +157,51 @@ function getTimeRemaining(endtime){
 
         // use classes for cards
 
-        // class MenuCard{
-        //     constructor(src, alt, title, descr, price, parentSelector, ...classes){
-        //         this.src = src;
-        //         this.alt = alt;
-        //         this.title = title;
-        //         this.descr = descr;
-        //         this.price = price;
-        //         this.classes = classes ;
-        
-        //         this.parent = document.querySelector(parentSelector);
-        //         this.transfer = 1;
-        //         this.changeToUAH();
-        //     }
+        class MenuCard{
+            constructor(src, alt, title, descr, price, parentSelector, ...classes){
+                this.src = src;
+                this.alt = alt;
+                this.title = title;
+                this.descr = descr;
+                this.price = price;
+                this.classes = classes ;
 
-        //     changeToUAH(){
-        //         this.price = this.price / this.transfer;
-        //     }
+                this.parent = document.querySelector(parentSelector);
+                this.transfer = 1;
+                this.changeToUAH();
+            }
 
-        //     render(){
-        //         const element = document.createElement('div');
+            changeToUAH(){
+                this.price = this.price / this.transfer;
+            }
 
-        //         if(this.classes.length === 0){
-        //             this.element = 'menu__item';
-        //             element.classList.add(this.element);
-        //         } else{
-        //             this.classes.forEach(className => element.classList.add(className));
-        //         }
+            render(){
+                const element = document.createElement('div');
+
+                if(this.classes.length === 0){
+                    this.element = 'menu__item';
+                    element.classList.add(this.element);
+                } else{
+                    this.classes.forEach(className => element.classList.add(className));
+                }
 
                 
-        //         element.innerHTML = `
+                element.innerHTML = `
                 
-        //             <img src=${this.src} alt=${this.alt}>
-        //             <h3 class="menu__item-subtitle">
-        //                 ${this.title}</h3>
-        //             <div class="menu__item-descr">${this.descr}</div>
-        //             <div class="menu__item-divider"></div>
-        //             <div class="menu__item-price">
-        //                 <div class="menu__item-cost">Price:</div>
-        //                 <div class="menu__item-total"><span>${this.price}</span> usd/day</div>
-        //             </div>
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">
+                        ${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Price:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> usd/day</div>
+                    </div>
                 
-        //         `;
-        //         this.parent.append(element);
-        //     }
-        // }
+                `;
+                this.parent.append(element);
+            }
+        }
 
         const getResource = async (url) => {
             const res = await fetch(url);
@@ -224,30 +224,38 @@ function getTimeRemaining(endtime){
 
         // another way by function
 
-        getResource('http://localhost:3000/menu')
-        .then(data => createCard(data));
+        // getResource('http://localhost:3000/menu')
+        // .then(data => createCard(data));
         
-        function createCard(data){
-            data.forEach(({img, altimg, title, descr, price}) =>{
-                const element = document.createElement('div');
+        // function createCard(data){
+        //     data.forEach(({img, altimg, title, descr, price}) =>{
+        //         const element = document.createElement('div');
                 
-                element.classList.add('menu__item');
+        //         element.classList.add('menu__item');
 
-                element.innerHTML = `
-                    <img src=${img} alt=${altimg}>
-                    <h3 class="menu__item-subtitle">
-                    ${title}</h3>
-                    <div class="menu__item-descr">${descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                    <div class="menu__item-cost">Price:</div>
-                    <div class="menu__item-total"><span>${price}</span> usd/day</div>
-                    </div>
-                `;
+        //         element.innerHTML = `
+        //             <img src=${img} alt=${altimg}>
+        //             <h3 class="menu__item-subtitle">
+        //             ${title}</h3>
+        //             <div class="menu__item-descr">${descr}</div>
+        //             <div class="menu__item-divider"></div>
+        //             <div class="menu__item-price">
+        //             <div class="menu__item-cost">Price:</div>
+        //             <div class="menu__item-total"><span>${price}</span> usd/day</div>
+        //             </div>
+        //         `;
 
-                document.querySelector('.menu .container').append(element);
+        //         document.querySelector('.menu .container').append(element);
+        //     });
+        // }
+
+
+        axios.get('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
+                        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
             });
-        }
+        });
 
         //forms
 
@@ -342,7 +350,205 @@ function getTimeRemaining(endtime){
             },4000);
         }
 
-        fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res));
+       
+
+        const slides = document.querySelectorAll('.offer__slide'),
+            slider = document.querySelector('.offer__slider'),
+            prev = document.querySelector('.offer__slider-prev'),
+            next = document.querySelector('.offer__slider-next'),
+            total = document.querySelector('#total'),
+            current = document.querySelector('#current'),
+            slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+            slidesField = document.querySelector('.offer__slider-inner'),
+            width = window.getComputedStyle(slidesWrapper).width;
+
+        let slideIndex = 1;
+        
+
+        // ver2 with carousel
+
+        let offset = 0;
+
+        if(slides.length < 10){
+            total.textContent = `0${slides.length}`;
+            current.textContent = `0${slideIndex}`;
+        } else {
+            total.textContent = slides.length;
+            current.textContent = slideIndex;
+        }
+
+        slidesField.style.width = 100 * slides.length + '%';
+        slidesField.style.display = 'flex';
+        slidesField.style.transition = '0.5s all';
+
+        slidesWrapper.style.overflow = 'hidden';
+
+        slides.forEach(slide => {
+            slide.style.width = width;
+        });
+
+        slider.style.position = 'relative';
+
+        const indicators = document.createElement('ol');
+            dots = [];
+
+        indicators.classList.add('carousel-indicators');
+        indicators.style.cssText = `
+            position: absolute;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            z-index: 15;
+            display: flex;
+            justify-content: center;
+            margin-right: 15%;
+            margin-left: 15%;
+            list-style: none;
+        `;
+        slider.append(indicators);
+
+        for(let i = 0; i < slides.length; i++){
+            const dot = document.createElement('li');
+            dot.setAttribute('data-slide-to', i + 1);
+            dot.style.cssText = `
+                box-sizing: content-box;
+                flex: 0 1 auto;
+                width: 30px;
+                height: 6px;
+                margin-right: 3px;
+                margin-left: 3px;
+                cursor: pointer;
+                background-color: #fff;
+                background-clip: padding-box;
+                border-top: 10px solid transparent;
+                border-bottom: 10px solid transparent;
+                opacity: .5;
+                transition: opacity .6s ease;
+            `;
+
+            if(i == 0){
+                dot.style.opacity = 1;
+            }
+
+            indicators.append(dot);
+            dots.push(dot);
+        }
+
+
+        next.addEventListener('click', ()=>{
+            if(offset === +width.slice(0, width.length - 2) * (slides.length - 1)){
+                offset = 0;
+            } else {
+                offset += +width.slice(0, width.length - 2);
+            }
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            if (slideIndex == slides.length){
+                slideIndex = 1;
+            } else {
+                slideIndex++;
+            }
+
+            if(slides.length < 10){
+                current.textContent = `0${slideIndex}`;
+            }else{
+                current.textContent = slideIndex;
+            }
+
+            dots.forEach(dot => dot.style.opacity = '.5');
+            dots[slideIndex - 1].style.opacity = 1;
+        });
+
+        prev.addEventListener('click', ()=>{
+            if(offset == 0){
+                offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            } else {
+                offset -= +width.slice(0, width.length - 2);
+            }
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            if (slideIndex == 1){
+                slideIndex = slides.length;
+            } else {
+                slideIndex--;
+            }
+
+            if(slides.length < 10){
+                current.textContent = `0${slideIndex}`;
+            }else{
+                current.textContent = slideIndex;
+            };
+
+
+            dots.forEach(dot => dot.style.opacity = '.5');
+            dots[slideIndex - 1].style.opacity = 1;
+            
+        });
+
+        dots.forEach(dot => {
+            dot.addEventListener('click', (e) => {
+                const slideTo = e.target.getAttribute('data-slide-to');
+
+                slideIndex = slideTo;
+                offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+                slidesField.style.transform = `translateX(-${offset}px)`;
+
+                if(slides.length < 10){
+                    current.textContent = `0${slideIndex}`;
+                }else{
+                    current.textContent = slideIndex;
+                };
+
+                dots.forEach(dot => dot.style.opacity = '.5');
+                dots[slideIndex - 1].style.opacity = 1;
+
+
+            });
+        })
+
+         // slider ver1
+
+
+        // showSlides(slideIndex);
+
+        // if(slides.length < 10){
+        //     total.textContent = `0${slides.length}`
+        // } else {
+        //     total.textContent = slides.length;
+        // }
+
+        // function showSlides (n) {
+        //     if (n > slides.length){
+        //         slideIndex = 1;
+        //     }
+
+        //     if ( n < 1){
+        //         slideIndex = slides.length;
+        //     }
+
+        //     slides.forEach(item => item.style.display = 'none');
+
+        //     slides[slideIndex - 1].style.display = 'block';
+
+        //     if(slides.length < 10){
+        //         current.textContent = `0${slideIndex}`
+        //     } else {
+        //         current.textContent = slideIndex;
+        //     }
+        // }
+            
+
+        // function plusSlides(n){
+        //     showSlides(slideIndex += n);
+        // }
+
+        // prev.addEventListener('click', ()=>{
+        //     plusSlides(-1);
+        // });
+        // next.addEventListener('click', ()=>{
+        //     plusSlides(1);
+        // });
 });
